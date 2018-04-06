0e56019a394ba358545011d38d3030c0820f1973
Anders Sildnes <anders.sildnes@cxense.com>
2018-04-06 15:03:41 +0200

    FORTUNE PROVIDES QUESTIONS FOR THE GREAT ANSWERS: #19 A:	To be or not to be. Q:	What is the square root of 4b^2?

--- make.sh
+++ make.sh
@@ -6,17 +6,26 @@
 # #3 try to keep only one instance of elipse alive
 
 # export ECLIM_ECLIPSE_HOME=/nix/store/isimibadhrlxg2vk54l3kjvyym4yccxj-eclipse-platform-4.7.2/eclipse/
-export ECLIM_ECLIPSE_HOME=$(echo $(ls -o "$(ls -o $(which eclipse) | sed 's/.*->//; s/bin.*// ; s/^ //')" | grep --color=none share | sed 's/.*->// ; s/^ // ; s#/share##')/eclipse)
+# export ECLIM_ECLIPSE_HOME=$(echo $(ls -o "$(ls -o $(which eclipse)/bin/eclipse | sed 's/.*->//; s/bin.*// ; s/^ //')" | grep --color=none share | sed 's/.*->// ; s/^ // ; s#/share##')/eclipse)
+# echo $ECLIM_ECLIPSE_HOME
+export ECLIM_ECLIPSE_HOME="$(ls -l $(nix-build '<myOverride>' --no-build-output -A myEclipse) | sed 's/.*->//; s/bin.*// ; s/^ // ; s#/share#/#' | grep --color=none platform )/eclipse"
 ECLIPSE_VERSION=$(awk -F = '/version/ { print $2 }' $ECLIM_ECLIPSE_HOME/.eclipseproduct)
-PLUGINS_DIR="$(find /nix/store/ -maxdepth 1 -name \*eclipse-plugins -type d)"
+# PLUGINS_DIR="$(find /nix/store/ -maxdepth 1 -name \*eclipse-plugins -type d)"
+PLUGINS_DIR="$(grep --color=none dropins "$(nix-build '<myOverride>' --no-build-output -A myEclipse | sed 's/.*->//; s/bin.*// ; s/^ // ; s#/share#/#' | grep --color=none platform)/etc/eclipse.ini" | sed 's/.*=//')"
+
+echo $ECLIM_ECLIPSE_HOME
+echo $ECLIPSE_VERSION
+echo $PLUGINS_DIR
+
 
 ant \
-  -Declipse.local=/home/andsild/.eclipse/org.eclipse.platform_$ECLIPSE_VERSION \
+  -Declipse.local=$HOME/.eclipse/org.eclipse.platform_$ECLIPSE_VERSION \
   -Dplugins.dir=$PLUGINS_DIR \
-  -Dvim.files=/home/andsild//.config/nvim # | grep -vE "\[javac\]"
+  -Dvim.files=$XDG_CONFIG_HOME/nvim \
+  || exit 1
 
-dir=$(find /home/andsild/.eclipse/org.eclipse.platform_$ECLIPSE_VERSION/plugins/ -name nailgun -type d)
+dir=$(find $HOME/.eclipse/org.eclipse.platform_$ECLIPSE_VERSION/plugins/ -name nailgun -type d)
 dir="${dir%/*}/bin"
 
-mv $dir/ng $dir/ng2
-ln -s $(which ng) $dir/ng
+mv -v $dir/ng $dir/ng2
+ln -vs $(which ng) $dir/ng
